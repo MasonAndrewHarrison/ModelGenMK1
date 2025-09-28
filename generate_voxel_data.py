442e9532f8f126be01b521c6b42a8157d59e6c29
MasonAndrewHarrison <masonandrewharrison@gmail.com>
2025-09-28 18:04:38 -0400

    update

--- generate_voxel_data.py
+++ generate_voxel_data.py
@@ -3,20 +3,22 @@ import numpy as np
 import time
 import render
 
+resolution = 50
+
 
 flower_dataset = Dataset.PointCloud()
 point_cloud_list = flower_dataset.load_data('normalized_dataset.npy')
 
 start = time.perf_counter()
 
-for index, m in enumerate(point_cloud_list, start=82):
+for index,_ in enumerate(point_cloud_list, start=0):
 
-    print(f"{index} out of 103")
+    print(f"{index+1} out of 103")
     model = point_cloud_list[index]
 
-    model = Dataset.converter_to_voxel(model, 100, 100, 100)
+    model = Dataset.converter_to_voxel(model, resolution, resolution, resolution)
 
-    directory = f"Voxel_Dataset_100p/model{index}.npy"
+    directory = f"Voxel_Dataset_{resolution}p/model{index}.npy"
     Dataset.save_data(directory, model)
 
 end = time.perf_counter()
